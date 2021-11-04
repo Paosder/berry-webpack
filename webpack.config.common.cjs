12ac85bdd8b545a865cfe0d05721bd912753a754
Paosder <denjaraos@gmail.com>
2021-11-04 20:48:55 +0900

    feat: improve splitting chunk

--- webpack.config.common.cjs
+++ webpack.config.common.cjs
@@ -72,8 +72,6 @@ module.exports = {
         }),
       ],
       splitChunks: {
-        chunks: "all",
-        enforceSizeThreshold: 5000,
         cacheGroups: {
           commons: {
             test: /[\\/]node_modules[\\/]/,
@@ -87,9 +85,17 @@ module.exports = {
                 .split("|")[0]
                 .replace(/.(js|css)/g, "");
               const allChunksNames = chunks.map((item) => item.name).join("~");
-              return `${cacheGroupKey}-${allChunksNames}-${moduleFileName}`;
+              return `${cacheGroupKey}-${moduleFileName}`;
             },
             chunks: "all",
+            reuseExistingChunk: true,
+          },
+          default: {
+            chunks: "all",
+            maxSize: 40960,
+            minChunks: 1,
+            reuseExistingChunk: true,
+            name: "main",
           },
         },
       },
