f38f01fbbf418f97e8ee0fa29ee1de5c837d396c
Paosder <denjaraos@gmail.com>
2021-11-05 17:07:11 +0900

    feat: add bundle-analyzer

--- webpack.config.production.cjs
+++ webpack.config.production.cjs
@@ -2,6 +2,7 @@
 const merge = require("webpack-merge");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const SpeedMeasurePlugin = require("speed-measure-webpack-plugin");
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
 const common = require("./webpack.config.common.cjs");
 
 const smp = new SpeedMeasurePlugin();
@@ -9,7 +10,14 @@ const smp = new SpeedMeasurePlugin();
 module.exports = smp.wrap(
   merge.merge(common.config, {
     devtool: false,
-    plugins: [new CleanWebpackPlugin()],
+    plugins: [
+      new CleanWebpackPlugin(),
+      new BundleAnalyzerPlugin({
+        openAnalyzer: false,
+        analyzerMode: "static",
+        reportFilename: "../report/index.html",
+      }),
+    ],
     stats: "minimal",
   })
 );
