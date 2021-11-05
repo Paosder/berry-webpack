0f169497c374ae713d71d210922d551ecb570924
Paosder <denjaraos@gmail.com>
2021-11-05 17:07:11 +0900

    feat: add bundle-analyzer

--- webpack.config.production.cjs
+++ webpack.config.production.cjs
@@ -1,10 +1,18 @@
 // production(build) config
 const merge = require("webpack-merge");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
 const common = require("./webpack.config.common.cjs");
 
 module.exports = merge.merge(common.config, {
   devtool: false,
-  plugins: [new CleanWebpackPlugin()],
+  plugins: [
+    new CleanWebpackPlugin(),
+    new BundleAnalyzerPlugin({
+      openAnalyzer: false,
+      analyzerMode: "static",
+      reportFilename: "../report/index.html",
+    }),
+  ],
   stats: "minimal",
 });
